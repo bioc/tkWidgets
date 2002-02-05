c4da7738ccd715b7de887569bb9a0318d876466d
jhnzhang <unknown>
2002-02-05 20:14:24 +0000

    *** empty log message ***
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@390 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fileBrowser.R
+++ R/fileBrowser.R
@@ -5,32 +5,122 @@
 fileBrowser <- function (path = ""){
 
     require(tcltk) || stop("tcl/tk library not available")
+    require(Biobase) || stop("Biobase library not available")
+
     LABELFONT <- "Helvetica 10"
     BUTWIDTH <- 8
 
     currentNode <- NULL
     nodes <- NULL
+    objLevel <- "Top Level"
+    fileOrObj <- "file"
+    readMode <- "readTable"
     fileSelected <- ""
+    obj <- NULL
 
     end <- function(){
         tkdestroy(base)
     }
 
-    inList <- function(){
+    viewFile <- function(){
+        writeCap(path)
+        writeDir(listView, list.files(path), path)
+        fileOrObj <<- "file"
+
+        if(currentNode >= 2)
+            tkconfigure(upBut, state = "normal")
+        else
+            tkconfigure(upBut, state = "disabled")
+    }
+
+    viewObj <- function(){
+        writeCap(paste("Object View:", objLevel[length(objLevel)]))
+        if(length(objLevel) == 1)
+            writeObj(listView, search())
+        else
+            writeObj(listView,
+                     ls(env = get(objLevel[length(objLevel)])))
+        fileOrObj <<- "obj"
+
+        if(length(objLevel) > 1)
+            tkconfigure(upBut, state = "normal")
+        else
+            tkconfigure(upBut, state = "disabled")
+    }
+
+    doEnv <- function (item){
+        fileOrObj <<- "obj"
+        writeObj(listView, ls(env = get(item)))
+        objLevel <<- c(objLevel, item)
+        writeCap(paste("Object View:", item))
+    }
+
+    doDF <- function (item){
+    }
+
+    doVec <- function(item){
+    }
+
+    doList <- function (item){
+        fileOrObj <<- "list"
+    }
+
+    doMat <- function (item){
+    }
+
+    doChar <- function (item){
+        fileOrObj <<- "character"
+
+        if(length(objLevel) > 1){
+            writeObj(listView, get(item,
+                                   env = get(objLevel[length(objLevel)])))
+            obj <<- get(item, env = get(objLevel[length(objLevel)]))
+        }else{
+            writeObj(listView, get(item))
+            obj <<- get(item)
+        }
+    }
+
+    showFiles <- function(){
         if(tkcurselection(listView) != ""){
             item <- tkget(listView, tkcurselection(listView))
             if(regexpr(Platform()$file.sep, item) >= 1){
 	        path <<- paste(path, Platform()$file.sep,
-                           gsub(Platform()$file.sep, "\\", item),
-                           sep = "")
+                               gsub(Platform()$file.sep, "\\", item),
+                               sep = "")
                 doPath()
                 writeDir(listView, list.files(path), path)
-	        writeCap(path)
+                writeCap(path)
                 if(currentNode >= 2)
                     tkconfigure(upBut, state = "normal")
             }else
-	        fileSelected <<- item
+                fileSelected <<- item
         }
+    }
+
+    showObj <- function (){
+        if(tkcurselection(listView) != ""){
+            item <- tkget(listView, tkcurselection(listView))
+            objType <- typeof(get(item))
+            print(paste("type = ", objType))
+            switch(objType,
+                   "environment" = doEnv(item),
+                   "data.frame" = doDF(item),
+                   "vector" = doVec(item),
+                   "list" = doList(item),
+                   "matrix" = doMat(item),
+                   "character" = doChar(item)
+                   )
+            if(length(objLevel) >= 2)
+                tkconfigure(upBut, state = "normal")
+        }
+    }
+
+    inList <- function(){
+        switch(fileOrObj,
+               "file" = showFiles(),
+               "obj" = showObj()
+           )
     }
 
     doPath <- function(){
@@ -38,8 +128,8 @@ fileBrowser <- function (path = ""){
         currentNode <<- length(nodes)
     }
 
-    up <- function(){
-	if(currentNode > 2){
+    fileUP <- function (){
+        if(currentNode > 2){
 	    path <<- paste(nodes[1:(currentNode - 1)],
 			  sep = "", collapse = Platform()$file.sep)
             writeDir(listView, list.files(path), path)
@@ -48,6 +138,27 @@ fileBrowser <- function (path = ""){
             if(currentNode == 2)
    	        tkconfigure(upBut, state = "disabled")
         }
+    }
+
+    objUP <- function(){
+        if(length(objLevel) > 2){
+            writeObj(listView,
+                     ls(env = get(objLevel[length(objLevel) - 1])))
+            writeCap(paste("Object View:",
+                           objLevel[length(objLevel) - 1]))
+            objLevel <<- objLevel[1:(length(objLevel) - 1)]
+        }else{
+            writeObj(listView, search())
+            writeCap(paste("Object View:", objLevel[1]))
+            tkconfigure(upBut, state = "disabled")
+        }
+    }
+
+    up <- function(){
+        switch(fileOrObj,
+               "file" = fileUP(),
+               "obj" = objUP(),
+               viewObj())
     }
 
     writeToView <- function(toWrite){
@@ -64,6 +175,22 @@ fileBrowser <- function (path = ""){
 
     base <- tktoplevel()
     tktitle(base) <- paste("File Browser")
+
+    topMenu <- tkmenu(base)
+    tkconfigure(base, menu = topMenu)
+
+    viewMenu <- tkmenu(topMenu, tearoff = FALSE)
+    tkadd(viewMenu, "command", label = "Files", command = viewFile)
+    tkadd(viewMenu, "command", label = "Work Space", command = viewObj)
+    tkadd(topMenu, "cascade", label = "View", menu = viewMenu)
+
+    readMenu <- tkmenu(topMenu, tearoff = FALSE)
+    tkadd(readMenu, "command", label = "read.csv")
+    tkadd(readMenu, "command", label = "read.delim")
+    tkadd(readMenu, "command", label = "readLines")
+    tkadd(readMenu, "command", label = "scan")
+    tkadd(readMenu, "command", label = "read.table")
+    tkadd(topMenu, "cascade", label = "Read", menu = readMenu)
 
     caption <- tklabel(base, text = path,font = LABELFONT,
                        width = 60)
@@ -87,8 +214,11 @@ fileBrowser <- function (path = ""){
 
     tkwait.window(base)
 
-    return(paste(path,  Platform()$file.sep,
+    if(fileOrObj == "file")
+        return(paste(path,  Platform()$file.sep,
                            fileSelected, sep = ""))
+    else
+        return(obj)
 }
 
 
