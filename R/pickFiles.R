7fcca575962d4ca325cdcf62e643b85520cf6abd
jhnzhang <unknown>
2002-03-09 15:29:46 +0000

    *** empty log message ***
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@633 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pickFiles.R
+++ R/pickFiles.R
@@ -1,32 +1,21 @@
-# This function gets a list of files from a folder passed and applies
-# a given function to each of them. If the function can be applied to
-# the file, the file is kept or left out otherwise. A vector
-# containing all the kept files will be returned.
+# This function checks a vector of file names from a directory and
+# determins what to be sent for desplay on the fileBrowser GUI based
+# on the default and user speficied parameters.
 #
 # Copyright 2002, J. Zhang. All rights reserved.
 #
-pickFiles <- function (funct,  path = ""){
-    on.exit(options(show.error.messages = TRUE))
-
-    filesPicked <- NULL
-    if(path == "")
-        path <- getwd()
-
-    filesGot <- list.files(path)
-
-    for(i in filesGot){
-        readLines(paste(path, Platform()$file.sep, i, sep = ""))
-
-        options(show.error.messages = FALSE)
-        checkMe <- try(funct(i))
-        options(show.error.messages = TRUE)
-
-        if(!inherits(checkMe, "try-error"))
-            filesPicked <- c(filesPicked,
-                             paste(path, Platform()$file.sep, i, sep = ""))
-    }
-
-    return(filesPicked)
+pickFiles <- function (fileNames, fun, prefix, suffix){
+    if(is.null(prefix) && is.null(suffix))
+       whichOnes <- sapply(fileNames, fun)
+    else
+       if(!is.null(prefix)){
+           tryMe <- hasPrefix(prefix)
+           whichOnes <- sapply(fileNames, tryMe)
+       }else{
+           tryMe <- hasSuffix(suffix)
+           whichOnes <- sapply(fileNames, tryMe)
+       }
+   return(fileNames[whichOnes])
 }
 
 
