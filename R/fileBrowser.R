10908ddb689c37be695719ca79be1ba4750a0eeb
jhnzhang <unknown>
2002-03-20 17:40:47 +0000

    Code modified to allow for selecting files from different folders and returning a vector of selected files upon exit
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@699 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fileBrowser.R
+++ R/fileBrowser.R
@@ -9,10 +9,18 @@ fileBrowser <- function (path = "", testFun = function(x) TRUE,
 
     LABELFONT <- "Helvetica 12"
     BUTWIDTH <- 8
+    CANWIDTH <- 500
+    CANHEIGHT <- 400
+    BOXHEIGHT <- 16
+    BOXWIDTH <- 29
+    OFFSET <- 10
+    LABELHEIGHT <- 20
 
     currentNode <- NULL
     nodes <- NULL
-    fileSelected <- ""
+    fileSelected <- NULL
+    currentDir <- NULL
+    currentFile <- NULL
 
     end <- function(){
         tkdestroy(base)
@@ -32,17 +40,47 @@ fileBrowser <- function (path = "", testFun = function(x) TRUE,
                 writeCap(path)
                 if(currentNode >= 2)
                     tkconfigure(upBut, state = "normal")
-                fileSelected <<- ""
             }
         }
     }
 
-    selFile <- function (){
+    selInDir <- function (){
         if(tkcurselection(listView) != ""){
-            selectedObj <<- tkget(listView, tkcurselection(listView))
-            fileSelected <<- tkget(listView,
-                                       tkcurselection(listView))
+            selObj <<- tkget(listView, tkcurselection(listView))
+            if(regexpr(".*/", selObj) == -1){
+                tkconfigure(selectBut, state = "normal")
+            }else{
+                tkconfigure(selectBut, state = "disabled")
+            }
         }
+    }
+
+    selectAFile <- function(){
+         if(tkcurselection(listView) != ""){
+             selObj <- tkget(listView, tkcurselection(listView))
+             if(regexpr(".*/", selObj) == -1){
+                 tkconfigure(selectBut, state = "normal")
+                 fileSelected <<- c(fileSelected,
+                                    paste(path, Platform()$file.sep,
+                                          selObj, sep = ""))
+                 fileSelected <<- unique(fileSelected)
+                 writeToView(selectView, fileSelected)
+             }else
+                 tkconfigure(selectBut, state = "disabled")
+         }
+    }
+
+    clearSelection <- function(){
+        writeToView(selectView, NULL)
+    }
+
+    dropSelection <- function(){
+         if(tkcurselection(selectView) != ""){
+             which <- tkindex(selectView, tkcurselection(selectView))
+             which <- as.numeric(which) + 1
+             fileSelected <<- fileSelected[-which]
+             writeToView(selectView, fileSelected)
+         }
     }
 
     doPath <- function(){
@@ -77,9 +115,10 @@ fileBrowser <- function (path = "", testFun = function(x) TRUE,
         }
     }
 
-    writeToView <- function(toWrite){
-  	tkdelete(listView, 0, "end")
-        tkinsert(listView, 0, toWrite)
+    writeToView <- function(aView, toWrite){
+  	tkdelete(aView, 0, "end")
+        if(!is.null(toWrite))
+            tkinsert(aView, 0,  gsub(".*/(.*)", "\\1", toWrite))
     }
 
     writeCap <- function(toWrite)
@@ -92,32 +131,68 @@ fileBrowser <- function (path = "", testFun = function(x) TRUE,
     base <- tktoplevel()
     tktitle(base) <- paste("File Browser")
 
-    caption <- tklabel(base, text = paste("Current:", path),
-                       font = LABELFONT, width = 60)
-    tkpack(caption, side = "top")
+    canvas <- tkcanvas(base, relief = "raised", width = CANWIDTH,
+                       height = CANHEIGHT)
+    tkpack(canvas, side = "top", fill = "both")
 
-    listFrame <- tkframe(base, height = 40)
-    listView <- makeView(listFrame)
-    tkpack(listFrame, fill = "x", expand = TRUE)
+    topFrame <- tkframe(canvas)
+    caption <- tklabel(topFrame, text = paste("Current:", path),
+                       font = LABELFONT)
+    tkpack(caption)
+    tkcreate(canvas, "window", (CANWIDTH/2), (OFFSET + LABELHEIGHT/2),
+	     anchor = "center", window = topFrame)
+
+    leftFrame <- tkframe(canvas)
+    dirLabel <- tklabel(leftFrame, text = "Files in directory",
+                        font = "Helvetica 10")
+    tkgrid(dirLabel, columnspan = 2)
+    viewFrame <- tkframe(leftFrame)
+    listView <- makeView(viewFrame, vWidth = BOXWIDTH,
+                         vHeight = BOXHEIGHT)
+    tkgrid(viewFrame, columnspan = 2)
+    upBut <- tkbutton(leftFrame, text = "Up", width = BUTWIDTH,
+		      command = up)
+    selectBut <- tkbutton(leftFrame, text = "Select >>", width = BUTWIDTH,
+		      command = selectAFile)
+    tkgrid(upBut, selectBut)
+    tkgrid.configure(upBut, sticky = "e")
+    tkgrid.configure(selectBut, sticky = "w")
 
     tkbind(listView, "<Double-Button-1>", inList)
-    tkbind(listView, "<B1-ButtonRelease>", selFile)
+    tkbind(listView, "<B1-ButtonRelease>", selInDir)
     writeDir(listView, pickFiles(dirsNFiles(path), testFun,
                                  prefix, suffix))
+    tkcreate(canvas, "window", OFFSET, (LABELHEIGHT + OFFSET),
+	     anchor = "nw", window = leftFrame)
 
-    butFrame <- tkframe(base)
-    upBut <- tkbutton(butFrame, text = "Up", width = BUTWIDTH,
-		      command = up)
+    rightFrame <- tkframe(canvas)
+    selLabel <- tklabel(rightFrame, text = "Files selected",
+                        font = "Helvetica 10")
+    tkgrid(selLabel, columnspan = 2)
+    sViewFrame <- tkframe(rightFrame)
+    selectView <- makeView(sViewFrame, vWidth = BOXWIDTH,
+                           vHeight = BOXHEIGHT)
+    tkgrid(sViewFrame, columnspan = 2)
+    remBut <- tkbutton(rightFrame, text = "<< Remove", width = BUTWIDTH,
+		      command = dropSelection)
+    clearBut <- tkbutton(rightFrame, text = "Clear", width = BUTWIDTH,
+		      command = clearSelection)
+    tkgrid(remBut, clearBut)
+    tkgrid.configure(remBut, sticky = "e")
+    tkgrid.configure(clearBut, sticky = "w")
+    tkcreate(canvas, "window", (CANWIDTH/2 + OFFSET), (LABELHEIGHT + OFFSET),
+	     anchor = "nw", window = rightFrame)
 
-    endBut <- tkbutton(butFrame, text = "End", width = BUTWIDTH,
+    botFrame <- tkframe(canvas)
+    endBut <- tkbutton(botFrame, text = "End", width = BUTWIDTH,
 		       command = end)
-    tkpack(upBut, endBut, side = "left")
-    tkpack(butFrame)
+    tkpack(endBut)
+    tkcreate(canvas, "window", CANWIDTH/2, (CANHEIGHT - OFFSET - LABELHEIGHT),
+	     anchor = "center", window = botFrame)
 
     tkwait.window(base)
 
-    return(paste(path, Platform()$file.sep, fileSelected,
-                 sep = ""))
+    return(fileSelected)
 }
 
 
