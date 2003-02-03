d7940c05265304a1e9f2264bfe4d0acc63135003
jhnzhang <unknown>
2003-02-03 16:45:37 +0000

    New function added and an old one removed.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@3219 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/dataViewer.R
@@ -0,0 +1,49 @@
+# This function creates a widget with a top frame that will filled
+# with a widget passed as one of the arguments and bottom frame with
+# buttons.
+#
+# Copyright 2002, J. Zhang. All right reserved.
+#
+dataViewer <- function(data, height = 20, width = 35, save = TRUE){
+
+    base <- tktoplevel()
+    tktitle(base) <- "BioC tkWidgets"
+
+    exit <- function() tkdestroy(base)
+
+    boxFrame <- tkframe(base)
+
+    # Insert data into the canvas
+    innerFrame <- makeViewer(boxFrame, vWidth = width, vHeight = height,
+                             vScroll = TRUE, side = "top",
+                             hScroll = TRUE, what = "text")
+    dataFrame <- tkframe(innerFrame)
+    for(i in 1:ncol(data)){
+        tempList <- tklistbox(dataFrame, width = 0, background = "white")
+        writeList(tempList, data[,i])
+        tkpack(tempList, side = "left")
+
+    }
+    tkwindow.create(innerFrame, "0.0", window = dataFrame)
+
+    tkpack(boxFrame, side = "top")
+
+    botFrame <- tkframe(base)
+    if(save){
+        save <- function(){
+            name <- argsWidget(list(FileName = ""))
+            write.table(data, name$FileName, quote = FALSE, sep = "\t",
+                        row.names = FALSE, col.names = FALSE)
+            exit()
+        }
+        saveBut <- tkbutton(botFrame, text = "Save", width = 8,
+                            command = save)
+        tkpack(saveBut, side = "left")
+    }
+    exitBut <- tkbutton(botFrame, text = "Exit", width = 8, command = exit)
+    tkpack(exitBut, side = "left")
+    tkpack(botFrame, side = "top")
+
+    tkwait.window(base)
+}
+
