5733f2bc1c5a21d1b2ad9859a6dd1ee95e7129a3
jhnzhang <unknown>
2003-02-26 13:56:43 +0000

    A new function added.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@3416 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getLightTW.R
@@ -0,0 +1,21 @@
+# A function to create a light wight tk widget with a text.
+# x and y - the location for the left upper coner of the widget to
+#           appear on the screen
+getLightTW <- function(text, x, y){
+    on.exit(end())
+
+    end <- function(){
+        tkdestroy(base)
+    }
+    # Takes out the frame and title bar
+    tkwm.overrideredirect(base <- tktoplevel(), TRUE)
+    # Put the TW in the right place
+    tkwm.geometry(base, paste("+", x, "+", y, sep = ""))
+    text <- tklabel(base, text = text)
+    tkpack(text)
+    tkbind(base, "<ButtonPress>", end)
+
+    tkwait.window(base)
+
+    return(invisible())
+}
