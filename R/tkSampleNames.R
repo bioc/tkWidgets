dd92647da397afc9336920d6999c95650ef651b5
jhnzhang <unknown>
2003-05-20 19:23:35 +0000

    Vegnette modified.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@4582 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tkSampleNames.R
+++ R/tkSampleNames.R
@@ -1,48 +1,48 @@
 tkSampleNames <- function(...,filenames=character(0)){
-  
-  
+
+
   auxnames <- as.list(substitute(list(...)))[-1]
   filenames <- .Primitive("c")(filenames, auxnames)
-  
+
   NumSamples <- length(filenames)
-  
+
   sample.names <- array("",NumSamples)
-  
-  for(i in 1:NumSamples){  
+
+  for(i in 1:NumSamples){
     s2 <-strsplit(strsplit(filenames[i],"/")[[1]][length(strsplit(filenames[i],"/")[[1]])],"\\.")[[1]]
     sample.names[i] <- s2[1]
   }
-  
+
   tt <- tktoplevel(width = 30)
   tkconfigure(tt, background = "white")
-  tktitle(tt) <- "Sapmle Information"
-  
+  tktitle(tt) <- "Sample Information"
+
   tbar.fr <- tkframe(tt,relief="sunken",borderwidth=5,width="5i")
   win.fr <- tkframe(tt,relief="sunken",borderwidth=5,width="5i",height="7i")
   tkconfigure(win.fr, background = "white")
   tkpack(tbar.fr,side="top",fill="x")
   tkpack(win.fr,side="top", fill="both")
-  
-  
+
+
   array.name.lst <- list()
   array.entry.lst <- list()
-  
-  
+
+
   var.lst <- list()
   for(i in 1:NumSamples) {
     var.lst[[i]] <-tclVar()
     tclvalue(var.lst[[i]]) <- sample.names[i]
   }
-  
+
   desc.lst <- list()
   for(i in 1:NumSamples) {
     desc.var <- tclVar("")
     desc.lst <- c(desc.lst,list(desc.var))
   }
-  
-  
+
+
   array.var <- list()
-  
+
   space.label1 <- tklabel(win.fr, text = "")
   tkconfigure(space.label1, background = "white")
   space.label2 <- tklabel(win.fr, text = "")
@@ -50,58 +50,58 @@ tkSampleNames <- function(...,filenames=character(0)){
   space.label3 <- tklabel(win.fr, text = "")
   tkconfigure(space.label3, background = "white")
   tkgrid(space.label1,row = 1)
-  
+
   array.name.lst <- tklabel(win.fr, text="File PathNames")
   tkconfigure(array.name.lst, background = "white")
-  tkgrid(array.name.lst, row = 2, col = 1)
-  
+  tkgrid(array.name.lst, row = 2, column = 1)
+
   array.entry.lst <- tklabel(win.fr, text="Sample Names")
   tkconfigure(array.entry.lst, background = "white")
-  tkgrid(array.entry.lst, row = 2, col = 2)
-  
+  tkgrid(array.entry.lst, row = 2, column = 2)
+
   desc.entry.lst <- tklabel(win.fr, text="Description")
   tkconfigure(desc.entry.lst, background = "white")
-  tkgrid(desc.entry.lst, row = 2, col = 3)
-  
-  
-  
+  tkgrid(desc.entry.lst, row = 2, column = 3)
+
+
+
   for(i in 1:NumSamples){
     array.name.lst[[i]] <-tklabel(win.fr, text=filenames[i])
     tkconfigure(array.name.lst[[i]], background = "white")
-    tkgrid(array.name.lst[[i]], row = (i+2), col = 1)
+    tkgrid(array.name.lst[[i]], row = (i+2), column = 1)
     tkgrid.configure(array.name.lst[[i]], sticky = "e")
-    
+
     array.entry.lst [[i]] <- tkentry(win.fr, width = 30, textvariable = var.lst[[i]])
     tkconfigure(array.entry.lst[[i]], background = "white")
-    tkgrid(array.entry.lst[[i]], row = (i+2), col = 2)
+    tkgrid(array.entry.lst[[i]], row = (i+2), column = 2)
     tkgrid.configure(array.entry.lst[[i]], sticky = "e")
-    
+
     desc.entry.lst [[i]] <- tkentry(win.fr, width = 30, textvariable = desc.lst[[i]])
     tkconfigure(desc.entry.lst[[i]], background = "white")
-    tkgrid(desc.entry.lst[[i]], row = (i+2), col = 3)
+    tkgrid(desc.entry.lst[[i]], row = (i+2), column = 3)
     tkgrid.configure(desc.entry.lst[[i]], sticky = "w")
-    
-    
-    
-    
+
+
+
+
   }
-  
-  
+
+
   tkgrid(space.label3,row=(NumSamples+7))
   finish.but <- tkbutton(win.fr, text = "Finish", command = function(){tkdestroy(tt)})
   tkconfigure(finish.but, background = "white")
-  tkgrid(finish.but, row = (NumSamples + 8), col = 2)
+  tkgrid(finish.but, row = (NumSamples + 8), column = 2)
   tkgrid.configure(finish.but, sticky="e")
-  
+
   tkwait.window(tt)
-  
+
   sam.vec <- matrix("",nrow=NumSamples,ncol=2)
   colnames(sam.vec) <- c("Sample Names","Description")
   for(i in 1:NumSamples){
     sam.vec[i,1] <- tclvalue(var.lst[[i]])
     sam.vec[i,2] <- tclvalue(desc.lst[[i]])}
-  return(sam.vec)                       
+  return(sam.vec)
 }
-                     
+
 
 
