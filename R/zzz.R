efcc65d45b2046ae1f638ca1b4ed0648b0b1dfc8
jhnzhang <unknown>
2003-11-05 19:03:42 +0000

    zzz.R added for adding vignettes to windows menu. JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@6520 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+
+.First.lib <- function(libname, pkgname, where) {
+
+    if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
+        && .Platform$GUI ==  "Rgui"){
+        addPDF2Vig("tkWidgets")
+    }
+
+}
