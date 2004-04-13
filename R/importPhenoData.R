81101662df85b1a14650a55ad4491bd3f6d86299
Jianhua Zhang <jzhang@jimmy.harvard.edu>
2004-04-13 14:01:47 +0000

    createPhenoData added.JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@8178 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/importPhenoData.R
+++ R/importPhenoData.R
@@ -1,7 +1,7 @@
 # Functions that quide users through the steps of inporting a
 # phenoData object. Used by the affy package.
 
-importPhenoData <- function(){
+importPhenoData <- function(sampleNames){
 
     fileName <- tclVar()
     objName <- tclVar()
@@ -9,6 +9,7 @@ importPhenoData <- function(){
     pdata <- NULL
     varLabels <- NULL
     phenodata <- NULL
+    newPhenoData <- NULL
 
     end <- function(){
          tkdestroy(base)
@@ -29,8 +30,9 @@ importPhenoData <- function(){
             args[["file"]] <- tclvalue(fileName)
             args <- argsWidget(args,
                                inst = "Arguments for function read.table")
+            args[["row.names"]] <- sampleNames
             pdata <<- do.call("read.table", as.list(args))
-            phenodata <<- createPhenoData(pdata)
+            newPhenodata <<- createPhenoData(pdata, sampleNames)
         }
     }
     readObj <- function(){
@@ -39,20 +41,24 @@ importPhenoData <- function(){
         }else{
             if(is.null(pdata)){
                 pdata <<- get(tclvalue(objName))
-                phenodata <<- createPhenoData(pdata)
             }
+            newPhenodata <<- createPhenoData(pdata, sampleNames)
         }
     }
     editObj <- function(){
-        if(tclvalue(phenoName) == ""){
-            showIOError("phenoData")
-        }else{
-            if(is.null(phenoData)){
-                phenodata <<- get(tclvalue(phenoName), env = .GlobalEnv)
+        #if(tclvalue(phenoName) == ""){
+        #    showIOError("phenoData")
+        #}else{
+            if(is.null(phenodata)){
+                if(tclvalue(phenoName) != ""){
+                    phenodata <<- get(tclvalue, env = .GlobalEnv)
+                    pdata <<- pData(phenodata)
+                }
+            }else{
+                pdata <<- pData(phenodata)
             }
-            pdata <<- pData(phenodata)
-            phenodata <- createPhenoData(pdata)
-        }
+            newPhenoData <<- createPhenoData(pdata, sampleNames)
+        #}
     }
     brows <- function(){
         tclvalue(fileName) <<- tclvalue(tkcmd("tk_getOpenFile"))
@@ -123,7 +129,7 @@ importPhenoData <- function(){
     editBut <- tkbutton(editFrame, text = "Edit/Create", width = 16,
                     command = editObj)
     tkpack(editBut, side = "left", expand = FALSE, padx = 5)
-    tkpack(tklabel(editFrame, text = "Object name:", width = 12),
+    tkpack(tklabel(editFrame, text = "phenoData name:", width = 12),
            side = "left", expand = FALSE)
     tkpack(tkentry(editFrame, width = 50, textvariable = phenoName),
            side = "left", expand = TRUE, fill = "x")
@@ -139,7 +145,7 @@ importPhenoData <- function(){
     tkwait.window(base)
 }
 
-
+# Shows an erro message when no name has been entered
 showIOError <- function(what){
     tkmessageBox(title = paste(what, "name missing"),
                  message = paste("Please enter a", what, "name first"),
@@ -148,10 +154,62 @@ showIOError <- function(what){
     return(invisible())
 }
 
-createPhenoData <- function(pdata){
-    pdata <- as.data.frame(pdata)
+createPhenoData <- function(pdata, sampleNames){
+    if(is.null(pdata)){
+        covarNum <- tclVar()
+    }else{
+        covarNum <- tclVar(ncol(pdata) - 1)
+    }
+
+    end <- function(){
+         tkdestroy(base)
+    }
+    on.exit(end())
+
+    cont <- function(){
+        end()
+    }
+
+    base <- tktoplevel()
+    tktitle(base) <- "BioC PhenoData Wizard"
+
+    tkpack(tklabel(base, text = paste("Please enter pheno data using",
+                        "the widgets below")),
+                  side = "top", expand = FALSE, padx = 5, pady = 5)
+    # Dropdown list for covariate numbers
+    covarDrop <- tkframe(base)
+    tkpack(tklabel(covarDrop, text = "Number of covariate:"),
+           side = "left", expand = FALSE)
+    drop <- tkframe(covarDrop)
+    dropdownList(drop, 1:20, covarNum, 15,
+                 ifelse(tclvalue(covarNum) == "", 1,
+                        tclvalue(covarNum)), TRUE)
+    tkpack(drop, side = "left", expand = TRUE, fill = "x")
+    tkpack(covarDrop, side = "top", expand = TRUE, fill = "x", padx = 5)
+
+    # A text widget to keep phenoData entries
+    dataFrame <- tkframe(base)
+    dataText <- makeViewer(dataFrame, vWidth = 30, vHeight = 15,
+                           hScroll = TRUE,
+                           vScroll = TRUE, what = "text", side = "left",
+                           text = "")
+    tkpack(dataText, side = "top", expand = TRUE, fill = "both")
+    tkpack(dataFrame, side = "top", expand = TRUE, fill = "both",
+           padx = 5)
+
+    butFrame <- tkframe(base)
+    backBut <- tkbutton(butFrame, text = "Back", width = 8, command = end)
+    contBut <- tkbutton(butFrame, text = "Continue", width = 8,
+                    command = cont)
+    tkgrid(backBut, contBut, padx = 20)
+    #tkgrid.configure(backBut, sticky = "w")
+    #tkgrid.configure(contBut, sticky = "e")
+    tkpack(butFrame, expand = FALSE, fill = "x", padx = 5, pady = 5)
+
+    tkwait.window(base)
 
 }
+
 
 
 
