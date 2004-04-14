271814ec0ad976e8a65e8fec0a24cd289f812ba0
Jianhua Zhang <jzhang@jimmy.harvard.edu>
2004-04-14 15:32:26 +0000

    More functions added to importPhenoData.JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@8200 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/importPhenoData.R
+++ R/importPhenoData.R
@@ -41,8 +41,11 @@ importPhenoData <- function(sampleNames){
                      icon = "error",
                      type = "ok")
             }else{
-                pdata <<- tryMe
-                newPhenodata <<- createPhenoData(pdata, sampleNames)
+                pdata <<- sNames4rNames(tryMe, sampleNames)
+                if(!is.null(pdata)){
+                    newPhenodata <<- createPhenoData(pdata, sampleNames)
+                    end()
+                }
             }
         }
     }
@@ -51,9 +54,13 @@ importPhenoData <- function(sampleNames){
             showIOError("data frame")
         }else{
             if(is.null(pdata)){
-                pdata <<- get(tclvalue(objName))
+                pdata <<- get(tclvalue(objName), env = .GlobalEnv)
             }
-            newPhenodata <<- createPhenoData(pdata, sampleNames)
+            pdata <- sNames4rNames(pdata, sampleNames)
+            if(!is.null(pdata)){
+                newPhenodata <<- createPhenoData(pdata, sampleNames)
+                end()
+            }
         }
     }
     editObj <- function(){
@@ -62,7 +69,7 @@ importPhenoData <- function(sampleNames){
         #}else{
             if(is.null(phenodata)){
                 if(tclvalue(phenoName) != ""){
-                    phenodata <<- get(tclvalue, env = .GlobalEnv)
+                    phenodata <<- get(tclvalue(phenoName), env = .GlobalEnv)
                     pdata <<- pData(phenodata)
                 }
             }else{
@@ -75,8 +82,8 @@ importPhenoData <- function(sampleNames){
         tclvalue(fileName) <<- tclvalue(tkcmd("tk_getOpenFile"))
     }
     browseObj <- function(){
-        filter <- function(x){
-            if(is.data.frame(get(x)))
+        filter <- function(x, env = .GlobalEnv){
+            if(is.data.frame(get(x, env = env)))
                 return(TRUE)
             else
                 return(FALSE)
@@ -152,8 +159,9 @@ importPhenoData <- function(sampleNames){
     tkpack(tkbutton(base, text = "Cancel", command = cancel, width = 15),
            side = "top", anchor = "center", expand = FALSE, pady = 10)
 
-
     tkwait.window(base)
+
+    return(newPhenoData)
 }
 
 # Shows an erro message when no name has been entered
@@ -165,11 +173,29 @@ showIOError <- function(what){
     return(invisible())
 }
 
+# Put sample names as row names of the data frame
+sNames4rNames <- function(pdata, sampleNames){
+    options(show.error.messages = FALSE)
+    tryMe <- try(rownames(pdata) <- sampleNames)
+    options(show.error.messages = TRUE)
+    if(inherits(tryMe, "try-error")){
+        ok <- tkmessageBox(title = "Sample mis-match",
+                     message = paste("Length of sample names and",
+                         "row numbers of data frame do not match"),
+                     icon = "error",
+                     type = "ok")
+        return(NULL)
+    }
+    return(tryMe)
+}
 createPhenoData <- function(pdata, sampleNames){
+    phenoObj <- NULL
+    phenoList <- NULL
+
     if(is.null(pdata)){
         covarNum <- tclVar()
     }else{
-        covarNum <- tclVar(ncol(pdata) - 1)
+        covarNum <- tclVar(ncol(pdata))
     }
 
     end <- function(){
@@ -178,14 +204,23 @@ createPhenoData <- function(pdata, sampleNames){
     on.exit(end())
 
     cont <- function(){
+        phenoObj <<- convert2PData(phenoList)
         end()
+    }
+    changeCovar <- function(){
+        phenoList <- writePhenoTable(base, dataText, pdata, sampleNames,
+                                 as.numeric(tclvalue(covarNum)))
     }
 
     base <- tktoplevel()
     tktitle(base) <- "BioC PhenoData Wizard"
 
     tkpack(tklabel(base, text = paste("Please enter pheno data using",
-                        "the widgets below")),
+                        "the table below\n\nThe first column shows sample",
+                         "names and the first row shows covariate names.",
+                         "\nEmpty cells right below covariate names are for",
+                         "entries for desciptions \nof covariates."),
+                   justify = "left"),
                   side = "top", expand = FALSE, padx = 5, pady = 5)
     # Dropdown list for covariate numbers
     covarDrop <- tkframe(base)
@@ -196,14 +231,15 @@ createPhenoData <- function(pdata, sampleNames){
                  ifelse(tclvalue(covarNum) == "", 1,
                         tclvalue(covarNum)), TRUE)
     tkpack(drop, side = "left", expand = TRUE, fill = "x")
+    tkpack(tkbutton(covarDrop, text = "Apply", width = 7,
+                    command = changeCovar), expand = FALSE)
     tkpack(covarDrop, side = "top", expand = TRUE, fill = "x", padx = 5)
 
     # A text widget to keep phenoData entries
     dataFrame <- tkframe(base)
-    dataText <- makeViewer(dataFrame, vWidth = 30, vHeight = 15,
+    dataText <- makeViewer(dataFrame, vWidth = 72, vHeight = 15,
                            hScroll = TRUE,
-                           vScroll = TRUE, what = "text", side = "left",
-                           text = "editable cells to be implemented")
+                           vScroll = TRUE, what = "text", side = "left")
     tkpack(dataText, side = "top", expand = TRUE, fill = "both")
     tkpack(dataFrame, side = "top", expand = TRUE, fill = "both",
            padx = 5)
@@ -213,13 +249,95 @@ createPhenoData <- function(pdata, sampleNames){
     contBut <- tkbutton(butFrame, text = "Continue", width = 8,
                     command = cont)
     tkgrid(backBut, contBut, padx = 20)
-    #tkgrid.configure(backBut, sticky = "w")
-    #tkgrid.configure(contBut, sticky = "e")
     tkpack(butFrame, expand = FALSE, fill = "x", padx = 5, pady = 5)
+
+    phenoList <- writePhenoTable(base, dataText, pdata, sampleNames,
+                                 as.numeric(tclvalue(covarNum)))
 
     tkwait.window(base)
 
+    return(phenoObj)
 }
+
+writePhenoTable <- function(base, textWidget, pdata, sampleNames,
+                            covarNum){
+    phenoMat <- makePhenoData(pdata, sampleNames, covarNum)
+    values <- list()
+    tkdelete(textWidget, "0.0", "end")
+    for(i in 1:nrow(phenoMat)){
+        tempList <- list()
+        for(j in 1:ncol(phenoMat)){
+            if(i == 1 || j == 1){
+                style <- "raised"
+            }else{
+                style <- "sunken"
+            }
+            tempList[[j]] <- tclVar(phenoMat[i, j])
+            tempEntry <- tkentry(base, textvariable = tempList[[j]],
+                                   width = 11, relief = style)
+            tkwindow.create(textWidget, "end", window = tempEntry)
+        }
+        tkinsert(textWidget, "end", "\n")
+        values[[i]] <- tempList
+    }
+    return(values)
+}
+
+makePhenoData <- function(pdata, sampleNames, covarNum){
+    if(!is.null(pdata)){
+        if(covarNum == ncol(pdata)){
+            temp <- rbind(c("", colnames(pdata)), rep("", nrow(pdata)),
+                          as.matrix(cbind(rownames(pdata), pdata)))
+        }else if(covarNum > ncol(pdata)){
+            temp <- rbind(c("", colnames(pdata), rep("", nrow(pdata)),
+                        paste("CoVar", (ncol(pdata)+1):covarNum, sep = "")),
+                          as.matrix(cbind(rownames(pdata),
+                         pdata, matrix("", ncol = covarNum - ncol(pdata),
+                                       nrow = length(sampleNames)))))
+        }else{
+            temp <- rbind(c("", colnames(pdata)[1:covarNum]),
+                          rep("", nrow(pdata)),
+                          as.matrix(cbind(rownames(pdata),
+                         pdata[, 1:covarNum])))
+
+        }
+    }else{
+        temp <- rbind(c("", paste("Covar", 1:covarNum, sep = "")),
+                      rep("", nrow(pdata)),
+                      as.matrix(cbind(sampleNames, matrix("",
+                      ncol = covarNum, nrow = length(sampleNames)))))
+    }
+    return(as.matrix(temp))
+}
+
+convert2PData <- function(phenoList){
+    pdata <- NULL
+    varlist <- list()
+    cnames <- NULL
+    for(i in 1:length(phenoList)){
+        temp <- phenoList[[i]]
+        tempP <- NULL
+        for(j in 1:length(temp)){
+            if(i > 2){
+                tempP <- c(tempP, tclvalue(temp[[j]]))
+            }else if (i == 2 && j != 1){
+                varlist[[length(varlist) + 1]] <- tclvalue(temp[[j]])
+            }else if(i == 1 && j != 1){
+                cnames <- c(cnames, tclvalue(temp[[j]]))
+            }
+        }
+        pdata <- rbind(pdata, tempP)
+    }
+    rnames <- pdata[,1]
+    pdata <- pdata[,2:ncol(pdata)]
+    rownames(pdata) <- rnames
+    colnames(pdata) <- cnames
+    names(varlist) <- as.character(1:length(varlist))
+
+    return(new("phenoData", pData=data.frame(pdata), varLabels=varlist))
+
+}
+
 
 
 
