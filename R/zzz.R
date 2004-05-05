2a6f709f3eb7a8c3b034ac280e43209e6b349b61
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-05-05 20:51:53 +0000

    switch to new win vig menu function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@8539 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 
     if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
         && .Platform$GUI ==  "Rgui"){
-        addPDF2Vig("tkWidgets")
+        addVigs2WinMenu("tkWidgets")
     }
 
 }
