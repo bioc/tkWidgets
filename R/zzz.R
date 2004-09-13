c9c2c7202848a047a399ba242f5980fb28663b45
Jianhua Zhang <jzhang@jimmy.harvard.edu>
2004-09-13 18:09:27 +0000

    .onLoad added and required changes made.JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@9923 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
 
-.First.lib <- function(libname, pkgname, where) {
-
+.onLoad <- function(libname, pkgname, where) {
+    require("methods", quietly=TRUE) || stop("Package methods unavailable")
+    require("widgetTools", quietly = TRUE) ||
+                     stop("Package widgetTools unavailable!")
     if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
         && .Platform$GUI ==  "Rgui"){
         addVigs2WinMenu("tkWidgets")
     }
-
 }
