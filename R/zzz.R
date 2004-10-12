36c7f710e6c14bf1f7978aa529d6c411f7aa4348
Robert Gentleman <rgentlem@fhcrc.org>
2004-10-12 20:08:13 +0000

    fixed a few different things...
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tkWidgets@10040 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
     require("methods", quietly=TRUE) || stop("Package methods unavailable")
     require("widgetTools", quietly = TRUE) ||
                      stop("Package widgetTools unavailable!")
+    require("DynDoc", quietly=TRUE) || stop("Package DynDoc unavailable")
     if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
         && .Platform$GUI ==  "Rgui"){
         addVigs2WinMenu("tkWidgets")
